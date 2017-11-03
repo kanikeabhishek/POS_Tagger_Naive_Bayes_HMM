ef0d5e46f08838e187c8edf20d9b275e519dc593
Abhishek Kanike <abkanike@iu.edu>
2017-11-03 04:50:18 -0400

    Calculate initial and transition probability.
    Also update simiplified prediction based on Bayes Law

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -12,7 +12,7 @@
 
 import random
 import math
-
+from collections import Counter
 
 # We've set up a suggested code structure, but feel free to change it. Just
 # make sure your code still works with the label.py and pos_scorer.py code
@@ -22,6 +22,7 @@ class Solver:
 
     P_word_pos = {}
     P_pos = {}
+    P_transition = {}
     ALL_POS = ['adj', 'adv', 'adp', 'conj', 'det', 'noun', 'num', 'pron', 'prt', 'verb', 'x', '.']
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
@@ -30,10 +31,26 @@ class Solver:
 
     # Do the training!
     #
+
+    def initialilze_transition_probability(self):
+        return {pos1: {pos2: 0 for pos2 in self.ALL_POS} for pos1 in self.ALL_POS}
+
+
     def train(self, data):
         number_of_words = 0
+        # Initialize transition probability
+        self.P_transition = self.initialilze_transition_probability()
+
+        # Calculate Initial probabilty
+        self.P_initial = Counter([pos_bag[0] for _, pos_bag in data])
+        for pos, count in self.P_initial.items():
+            self.P_initial[pos] /= float(len(data))
+
+        # Count for prior and conditional probabilities
         for (sentence, pos_bag) in data:
             for item in range(0, len(sentence)):
+                if item > 0:
+                    self.P_transition[pos_bag[item]][pos_bag[item-1]] += 1
                 number_of_words += 1
                 if pos_bag[item] in self.P_pos:
                     self.P_pos[pos_bag[item]] += 1
@@ -46,9 +63,16 @@ class Solver:
                 else:
                     self.P_word_pos[pos_bag[item]][sentence[item]] = 1
 
+        # Calculate Transition probabilities of pos as hash of hash of pos
+        for cur_pos in self.P_transition:
+            total = sum(self.P_transition[cur_pos].values())
+            for next_pos in self.P_transition[cur_pos]:
+                self.P_transition[cur_pos][next_pos] /= total
+
+        # Caluclate Emission probabilities of word | pos as hash of pos of word
         for gt_pos in self.P_pos:
             self.P_pos[gt_pos] /= float(number_of_words)
-            total_words_pos = sum([self.P_word_pos[gt_pos][word] for word in self.P_word_pos[gt_pos]])
+            total_words_pos = sum([count for word, count in self.P_word_pos[gt_pos].items()])
             for word in self.P_word_pos[gt_pos]:
                 self.P_word_pos[gt_pos][word] /= float(total_words_pos)
 
@@ -60,7 +84,10 @@ class Solver:
             (max_prob, max_pos) = (0, '')
             for cur_pos in self.ALL_POS:
                 if word in self.P_word_pos[cur_pos]:
-                    (max_prob, max_pos) = max((max_prob, max_pos), (self.P_word_pos[cur_pos][word], cur_pos))
+                    numerator = self.P_word_pos[cur_pos][word] * self.P_pos[cur_pos]
+                    denominator = (self.P_word_pos[cur_pos][word] * self.P_pos[cur_pos]) \
+                                + ((1 - self.P_word_pos[cur_pos][word]) * (1 - self.P_pos[cur_pos]))
+                    (max_prob, max_pos) = max((max_prob, max_pos), (numerator/denominator, cur_pos))
             sentence_pos.append(max_pos)
         return sentence_pos
 
